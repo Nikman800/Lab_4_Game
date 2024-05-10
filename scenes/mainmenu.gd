a48880b1a1b5b6e145409e570c0df168c1ae861b
BluePhoenix266 <153779459+BluePhoenix266@users.noreply.github.com>
2024-05-10 16:22:05 -0700

    added controls to menu

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -3,11 +3,11 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-
 func _on_quit_pressed():
 	get_tree().quit
-	
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
